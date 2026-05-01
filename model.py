1a2217eee0f0533faf09f34c779169fafcb81268
SAI SAHARSH <saharshps099@gmail.com>
2026-05-02 01:02:36 +0530

    Modify KNeighborsClassifier and enhance evaluation metrics
    Updated KNeighborsClassifier to use 3 neighbors and added cross-validation scores for various strategies.

--- model.py
+++ model.py
@@ -21,7 +21,7 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,
                                                random_state=42)
 
 from sklearn.neighbors import KNeighborsClassifier
-knn=KNeighborsClassifier(n_neighbors=2)
+knn=KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train,y_train)
 
 print(knn.score(X_test,y_test))
